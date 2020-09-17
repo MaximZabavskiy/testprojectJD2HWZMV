1f6a679bf23cdbc8d57355797e7927bcb89c53db
MaximZabavskiy <zabavskiy.mv@gmail.com>
2020-09-17 15:01:57 +0300

    Hello, world!1111111

--- src/main/java/com/noirix/Main.java
+++ src/main/java/com/noirix/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, world!");
         System.out.println("Hello, world!1111");
+        System.out.println("Hello, world!1111111");
         System.out.println("String for cherry pick");
         System.out.println("Dev changes!");
     }
