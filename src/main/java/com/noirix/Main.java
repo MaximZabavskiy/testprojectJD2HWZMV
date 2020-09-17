57a8a54288667e7d516eec7ff2ff0c5ea5347ae9
MaximZabavskiy <zabavskiy.mv@gmail.com>
2020-09-17 10:39:17 +0300

    Cherry pick test

--- src/main/java/com/noirix/Main.java
+++ src/main/java/com/noirix/Main.java
@@ -9,6 +9,6 @@ public class Main {
 
         System.out.println("Hello, world!");
         System.out.println("Hello, world!1111");
-        System.out.println("Hello, world!11111111");
+        System.out.println("String for cherry pick");
     }
 }
